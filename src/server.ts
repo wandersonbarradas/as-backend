64f84f5accd1ea77308360b4b0a15b6c2dda362a
WandersonBarradas <wandersonbarradas07@gmail.com>
2024-01-12 18:14:36 -0300

    add config deploy

--- src/server.ts
+++ src/server.ts
@@ -17,15 +17,16 @@ app.all("*", requestIntercepter);
 app.use("/admin", adminRoutes);
 app.use("/", siteRoutes);
 
-// const runServer = (port: number, server: http.Server) => {
-//     server.listen(port, () => {
-//         console.log("🚀 Running at PORT", port);
-//     });
-// };
-// const regularServer = http.createServer(app);
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, () => {
+        console.log("🚀 Running at PORT", port);
+    });
+};
+const regularServer = http.createServer(app);
 // if (process.env.NODE_ENV === "production") {
 // } else {
 //     const serverPort = process.env.PORT ? parseInt(process.env.PORT) : 9000;
 //     runServer(serverPort, regularServer);
 // }
-app.listen(process.env.PORT);
+const serverPort = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+runServer(serverPort, regularServer);
