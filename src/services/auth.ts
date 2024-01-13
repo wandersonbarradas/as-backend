738d6df07b1c40812cdcd12d4434ed803ae7a922
WandersonBarradas <wandersonbarradas07@gmail.com>
2024-01-12 21:14:36 -0300

    debugg

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,6 +2,8 @@ import { getToday } from "../utils/getToday";
 
 export const validatePassword = (password: string): boolean => {
     const currentPassword = getToday().split("/").join("");
+    console.log("🚀 ~ validatePassword ~ password:", password);
+    console.log("🚀 ~ validatePassword ~ currentPassword:", currentPassword);
     return currentPassword === password;
 };
 
